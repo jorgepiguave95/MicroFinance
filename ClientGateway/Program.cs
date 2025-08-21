4943c1ddb0f975f73c8fc8a9fbfe4a4dff3b119f
Jorge Piguave <jorgepiguave95@live.com>
2025-08-20 20:56:15 -0500

    Add HTTP client configurations and API endpoints for customers and accounts; update Dockerfiles and .dockerignore files for all services

--- ClientGateway/Program.cs
+++ ClientGateway/Program.cs
@@ -1,18 +1,35 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddHttpClient("customers", c =>
+    c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("CUSTOMERS_BASEURL")!));
+builder.Services.AddHttpClient("accounts", c =>
+    c.BaseAddress = new Uri(Environment.GetEnvironmentVariable("ACCOUNTS_BASEURL")!));
+
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
+
+//Proxies
+app.MapGet("/api/customers/{id}", async (string id, IHttpClientFactory f) =>
+{
+    var http = f.CreateClient("customers");
+    var r = await http.GetAsync($"/api/customers/{id}");
+    return Results.Content(await r.Content.ReadAsStringAsync(), "application/json");
+});
+
+app.MapGet("/api/accounts/{id}", async (string id, IHttpClientFactory f) =>
+{
+    var http = f.CreateClient("accounts");
+    var r = await http.GetAsync($"/api/accounts/{id}");
+    return Results.Content(await r.Content.ReadAsStringAsync(), "application/json");
+});
 
 app.UseHttpsRedirection();
 
